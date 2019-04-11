bafff5dce934eba00666b3992226638f740e21ec
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-04-10 20:08:14 -0400

    update transparent

---
+++ cycles/demo_transparent.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append('/Users/hsuehtil/Dropbox/BlenderToolbox/cycles')
+from include import *
+import bpy
+
+outputPath = './results/demo_transparent.png'
+
+# # init blender
+imgRes_x = 720 # should set to > 2000 for paper figures
+imgRes_y = 720 # should set to > 2000 for paper figures
+numSamples = 100 # usually set to > 250 for high quality paper images
+exposure = 1.0 # need to double check
+blenderInit(imgRes_x, imgRes_y, numSamples, exposure)
+
+# read mesh 
+meshPath = '../meshes/spot.ply'
+location = (-0.3, 0.6, -0.04)
+rotation = (90, 0,0)
+scale = (1.5,1.5,1.5)
+mesh = readPLY(meshPath, location, rotation, scale)
+
+# # set shading
+bpy.ops.object.shade_smooth()
+# bpy.ops.object.shade_flat()
+
+# # subdivision
+level = 2
+subdivision(mesh, level)
+
+# # set material
+# colorObj(RGBA, H, S, V, Bright, Contrast)
+meshColor = colorObj(derekBlue, 0.5, 1.5, 1.0, 0.0, 0.0)
+transparency = 0.7
+setMat_transparent(mesh, meshColor, transparency)
+
+# # set invisible plane (shadow catcher)
+groundCenter = (0,0,0)
+shadowDarkeness = 0.7
+groundSize = 20
+invisibleGround(groundCenter, groundSize, shadowDarkeness)
+
+# # set camera
+camLocation = (1.9,2,2.2)
+lookAtLocation = (0,0,0.5)
+focalLength = 45
+cam = setCamera(camLocation, lookAtLocation, focalLength)
+
+# # set sunlight
+lightAngle = (-15,-34,-155) 
+strength = 2
+shadowSoftness = 0.1
+sun = setLight_sun(lightAngle, strength, shadowSoftness)
+
+# # set ambient light
+ambientColor = (0.2,0.2,0.2,1)
+setLight_ambient(ambientColor)
+
+# # save blender file
+bpy.ops.wm.save_mainfile(filepath='./test.blend')
+
+# # save rendering
+bpy.data.scenes['Scene'].render.filepath = outputPath
+bpy.data.scenes['Scene'].camera = cam
+bpy.ops.render.render(write_still = True)
--- cycles/include.py
+++ cycles/include.py
@@ -21,6 +21,7 @@ from utils.setMat_ceramic import *
 from utils.setMat_edge import *
 from utils.setMat_glass import *
 from utils.setMat_singleColor import *
+from utils.setMat_transparent import *
 from utils.setMat_pointCloud import *
 from utils.setMat_VColor import *
 from utils.setMat_monotone import *
