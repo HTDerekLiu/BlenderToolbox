0c6510f493089aa7bc07751ac18c2d7a6a282250
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-06-18 17:31:31 -0700

    a small bug for blender 2.8 update

--- cycles/utils/setLight_sun.py
+++ cycles/utils/setLight_sun.py
@@ -8,12 +8,14 @@ def setLight_sun(rotation_euler, strength, shadow_soft_size = 0.05):
 	angle = (x,y,z)
 	bpy.ops.object.light_add(type = 'SUN', rotation = angle)
 	lamp = bpy.data.lights['Sun']
+	lamp.use_nodes = True
 	lamp.shadow_soft_size = shadow_soft_size
-	
+
 	# robust to EEVEE or CYCLES
 	if bpy.context.scene.render.engine.title() == 'Blender_Eevee':
 		lamp.energy = strength
 	else:
 		lamp.node_tree.nodes["Emission"].inputs['Strength'].default_value = strength
+
 
 	return lamp
