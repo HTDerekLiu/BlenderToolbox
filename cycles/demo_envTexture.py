f9ced02845c8efd384fd9fa968c606da7667ea3c
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-12-01 19:36:15 -0500

    add different side

--- cycles/demo_envTexture.py
+++ cycles/demo_envTexture.py
@@ -3,13 +3,13 @@ sys.path.append('/Users/hsuehtil/Dropbox/BlenderToolbox/cycles')
 from include import *
 import bpy
 
-outputPath = './results/demo_threePtLights.png'
+outputPath = './results/demo_amber.png'
 
 # # init blender
 imgRes_x = 720 
 imgRes_y = 720 
 numSamples = 50 
-exposure = 1.5
+exposure = 1.0
 blenderInit(imgRes_x, imgRes_y, numSamples, exposure)
 
 # read mesh 
@@ -29,33 +29,31 @@ subdivision(mesh, level)
 
 # # set material
 # colorObj(RGBA, H, S, V, Bright, Contrast)
-meshColor = colorObj(derekBlue, 0.5, 1.0, 1.0, 0.0, 2.0)
-AOStrength = 0.5
-setMat_singleColor(mesh, meshColor, AOStrength)
+amberRGBA = (100/255.0,75/255.0,0/255.0,255/255.0) 
+meshColor = colorObj(amberRGBA, 0.5, 1.0, 1.0, 0.4, 0.0)
+setMat_amber(mesh, meshColor)
 
 # # set invisible plane (shadow catcher)
 groundCenter = (0,0,0)
-shadowBrightness = 0.8
+shadowDarkeness = 0.7
 groundSize = 20
-invisibleGround(groundCenter, groundSize, shadowBrightness)
+invisibleGround(groundCenter, groundSize, shadowDarkeness)
 
 # # set camera
-camLocation = (2,2,2)
+camLocation = (1.9,2,2.2)
 lookAtLocation = (0,0,0.5)
 focalLength = 45
 cam = setCamera(camLocation, lookAtLocation, focalLength)
 
-# set three lighting system
-keyLoc = 'right'
-setLight_threePoints(radius=4, height=10, intensity=1700, softness=6, keyLoc=keyLoc)
+# # set sunlight
+lightAngle = (-15,-34,-155) 
+strength = 2
+shadowSoftness = 0.1
+sun = setLight_sun(lightAngle, strength, shadowSoftness)
 
 # # set ambient light
-ambientColor = (0.1,0.1,0.1,1)
+ambientColor = (0.2,0.2,0.2,1)
 setLight_ambient(ambientColor)
-
-## set gray shadow to completely white with a threshold (optional)
-alphaThreshold = 0.05
-shadowThreshold(alphaThreshold, interpolationMode = 'CARDINAL')
 
 # # save blender file
 bpy.ops.wm.save_mainfile(filepath='./test.blend')
--- cycles/tutorial.py
+++ cycles/tutorial.py
@@ -67,7 +67,7 @@ cam = setCamera(camLocation, lookAtLocation, focalLength)
 
 ## set light
 ## Option1: Three Point Light System (recommended)
-setLight_threePoints(radius=4, height=10, intensity=1700, softness=6)
+setLight_threePoints(radius=4, height=10, intensity=1700, softness=6, keyLoc='left')
 ## Option2: simple sun light
 # lightAngle = (-15,-34,-155) 
 # strength = 2
