f4d90eedaf9bfe55c66817837ba6053dd49cc9ed
HTDerekLiu <hsuehtil@cs.toronto.edu>
2019-12-04 15:45:51 -0500

    update default params to use GPU only (thanks Rahul)

--- cycles/utils/blenderInit.py
+++ cycles/utils/blenderInit.py
@@ -1,6 +1,6 @@
 import bpy
 
-def blenderInit(resolution_x, resolution_y, numSamples = 128, exposure = 1.5):
+def blenderInit(resolution_x, resolution_y, numSamples = 128, exposure = 1.5, useBothCPUGPU = False):
 	# clear all
 	bpy.ops.wm.read_homefile()
 	bpy.ops.object.select_all(action = 'SELECT')
@@ -11,17 +11,23 @@ def blenderInit(resolution_x, resolution_y, numSamples = 128, exposure = 1.5):
 	bpy.context.scene.render.resolution_y = resolution_y 
 	# bpy.context.scene.cycles.film_transparent = True
 	bpy.context.scene.render.film_transparent = True
-	bpy.context.scene.cycles.device = 'GPU'
 	bpy.context.scene.cycles.samples = numSamples 
 	bpy.context.scene.cycles.max_bounces = 6
 	bpy.context.scene.cycles.film_exposure = exposure
 	bpy.data.scenes[0].view_layers['View Layer']['cycles']['use_denoising'] = 1
 
-	# set GPU devices
+	# set devices
 	cyclePref  = bpy.context.preferences.addons['cycles'].preferences
 	cyclePref.compute_device_type = 'CUDA'
 	for dev in cyclePref.devices:
-		dev.use = True
+		if dev.type == "CPU" and useBothCPUGPU is False:
+			dev.use = False
+		else:
+			dev.use = True
+	bpy.context.scene.cycles.device = 'GPU'
+
+	for dev in cyclePref.devices:
 		print (dev)
 		print (dev.use)
-	bpy.context.scene.cycles.device = 'GPU'
+
+	return 0
