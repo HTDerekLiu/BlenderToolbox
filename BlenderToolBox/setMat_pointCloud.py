8f2c59ae7b22408bca7f748bf4028345137de7b3
HTDerekLiu <hsuehtil@cs.toronto.edu>
2022-07-21 10:48:42 -0400

    change setmat point cloud to handle blender 3.2.1

--- BlenderToolBox/setMat_pointCloud.py
+++ BlenderToolBox/setMat_pointCloud.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import bpy
+from mathutils import Vector
 
 def setMat_pointCloud(mesh, \
                 meshColor, \
@@ -62,7 +63,12 @@ def setMat_pointCloud(mesh, \
     bpy.context.view_layer.objects.active = mesh
 
     bpy.ops.object.modifier_add(type='NODES')
-    tree = mesh.modifiers[-1].node_group
+    if mesh.modifiers[-1].node_group:
+        tree = curve.modifiers[-1].node_group    
+    else:
+        tree = new_GeometryNodes_group()
+        mesh.modifiers[-1].node_group = tree
+
     IN = tree.nodes['Group Input']
     OUT = tree.nodes['Group Output']
     MESH2POINT = tree.nodes.new('GeometryNodeMeshToPoints')
@@ -78,38 +84,16 @@ def setMat_pointCloud(mesh, \
     MATERIAL.inputs[2].default_value = mat
 
 
-    # # initialize a primitive sphere
-    # bpy.ops.mesh.primitive_uv_sphere_add(radius = 1.0, location = (1e7,1e7,1e7))
-    # sphere = bpy.context.object
-    # bpy.ops.object.shade_smooth()
-    # mat = bpy.data.materials.new(name="sphereMat")
-    # sphere.data.materials.append(mat)
-    # sphere.active_material = mat
-    # mat.use_nodes = True
-    # tree = mat.node_tree
-    # HSVNode = tree.nodes.new('ShaderNodeHueSaturation')
-    # HSVNode.inputs['Color'].default_value = ptColor.RGBA
-    # HSVNode.inputs['Saturation'].default_value = ptColor.S
-    # HSVNode.inputs['Value'].default_value = ptColor.V
-    # HSVNode.inputs['Hue'].default_value = ptColor.H
-
-    # # set color brightness/contrast
-    # BCNode = tree.nodes.new('ShaderNodeBrightContrast')
-    # BCNode.inputs['Bright'].default_value = ptColor.B
-    # BCNode.inputs['Contrast'].default_value = ptColor.C
-    # tree.links.new(HSVNode.outputs['Color'], BCNode.inputs['Color'])
-    # tree.links.new(BCNode.outputs['Color'], tree.nodes['Principled BSDF'].inputs['Base Color'])
-
-    # # init particle system
-    # mesh.modifiers.new("part", type='PARTICLE_SYSTEM')
-    # ps = mesh.particle_systems[0]
-    # ps.settings.count = len(mesh.data.vertices)
-    # ps.settings.frame_start = 0
-    # ps.settings.frame_end = 0
-    # ps.settings.emit_from = 'VERT'
-    # ps.settings.physics_type = 'NO'
-    # ps.settings.particle_size = ptSize
-    # ps.settings.render_type = 'OBJECT'
-    # ps.settings.instance_object = sphere
-    # ps.settings.use_emit_random = False
-
+def new_GeometryNodes_group():
+    ''' Create a new empty node group that can be used
+        in a GeometryNodes modifier.
+    '''
+    node_group = bpy.data.node_groups.new('GeometryNodes', 'GeometryNodeTree')
+    inNode = node_group.nodes.new('NodeGroupInput')
+    inNode.outputs.new('NodeSocketGeometry', 'Geometry')
+    outNode = node_group.nodes.new('NodeGroupOutput')
+    outNode.inputs.new('NodeSocketGeometry', 'Geometry')
+    node_group.links.new(inNode.outputs['Geometry'], outNode.inputs['Geometry'])
+    inNode.location = Vector((-1.5*inNode.width, 0))
+    outNode.location = Vector((1.5*outNode.width, 0))
+    return node_group
